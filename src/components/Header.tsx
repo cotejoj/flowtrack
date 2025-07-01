1d018f621c03da864e904478bc5c97ba02869d21
Jomar Cotejo <jomarcotejo@gmail.com>
2025-07-01 19:40:33 +1000

    update header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,10 @@
+import { FaProjectDiagram } from "react-icons/fa";
 import "./Header.css";
 
 export default function Header() {
   return (
     <header className="app-header">
+      <FaProjectDiagram className="header-icon" />
       <h1>FlowTrack</h1>
     </header>
   );
